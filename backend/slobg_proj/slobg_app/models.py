f293247eb22fc192a50bae7dec0d5153bc23479f
Dhaval Panchal <dhaval@Dhavals-MacBook-Pro.local>
2019-11-17 13:02:03 -0800

    Created Volunteer Model

--- backend/slobg_proj/slobg_app/models.py
+++ backend/slobg_proj/slobg_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Volunteer(models.Model):
+	total_hours = models.FloatField()
+	email = models.EmailField(max_length=254)
+	username = models.CharField(max_length=200)
+
+	def __str__(self):
+		return "Username: {}, Email: {}".format(self.username, self.email)
