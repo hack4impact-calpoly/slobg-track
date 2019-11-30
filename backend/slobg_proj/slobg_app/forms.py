40b292db99720e6909fc3042eae89b2e2e7a3f7d
jpoist97 <jpoist97@gmail.com>
2019-11-29 20:57:27 -0800

    Created Individual volunteer hour tracking form, VolunteerHoursForm ModelForm, VolunteerHours Model, add_ind_hours.html

--- backend/slobg_proj/slobg_app/forms.py
+++ backend/slobg_proj/slobg_app/forms.py
@@ -1,15 +1,63 @@
-from .models import VolunteerForm, Volunteer
+from .models import Volunteer, VolunteerHours
+from django import forms
 
 #Form for creating a new Volunteer model
 #Form reflects the Individual Volunteer form on the SRS
-class Volunteer(forms.ModelForm):
+class VolunteerForm(forms.ModelForm):
     class Meta:
         model = Volunteer
         fields = ['name', 'street_address', 'city', 'zipcode', 'state', 
                 'phone', 'email', 'birthdate', 'work_preference', 
                 'area_of_interest']
 
-class VolunteerForm(forms.ModelForm):
+
+#class VolunteerForm(forms.ModelForm):
+#    class Meta:
+#        model = Volunteer
+#        fields = ['username', 'name', 'street_address', 'city', 'zipcode', 
+#                'state', 'phone', 'email', 'birthdate', 'work_preference', 
+#                'area_of_interest']
+#       widgets = {
+#            'username' : forms.TextInput(
+#                attrs={
+#                    'class': 'form-control',
+#                    'placeholder': 'username'
+#                }),
+#            'name' : forms.TextInput(
+#                attrs={
+#                    'class': 'form-control',
+#                    'placeholder': 'First Last'
+#                }),
+#            'birthdate' : forms.TextInput(attrs={'class': 'form-control'})
+#        }
+
+class VolunteerHoursForm(forms.ModelForm):
     class Meta:
-        model = VolunteerForm
-        fields = ['activity','hours','date']
+        model = VolunteerHours
+        fields = ['date', 'hours', 'supervisor', 'activity']
+        widgets = {
+            'date':forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'yyyy-mm-dd'
+                }
+            ),
+            'hours':forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'0'
+                }
+            ),
+            'supervisor':forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'Supervisor Name'
+                }
+            ),
+            'activity':forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'Describe the activity you volunteered for'
+                }
+            ),
+        }
