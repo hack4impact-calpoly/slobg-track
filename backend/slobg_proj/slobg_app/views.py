6bb41f56f9bb20bf3d39f70227e88117c0b8845a
Eric <enewcomer23@gmail.com>
2020-01-15 12:43:05 -0800

    Add signup functionality

--- backend/slobg_proj/slobg_app/views.py
+++ backend/slobg_proj/slobg_app/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response
 
 from .forms import VolunteerHoursForm, GroupVolunteerForm
 from .models import VolunteerHours, GroupVolunteerModel
 
-# Create your views here.
+# Login stuff
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def signup(request):
+   if request.method == "POST":
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         raw_password = form.cleaned_data.get('password1')
+         user = authenticate(username=username, password=raw_password)
+         login(request, user)
+         return redirect('/')
+      else:
+         print("form not valid")
+   else:
+      print("not POST")
+      form = UserCreationForm()
+   return render(request, "slobg_app/signup.html", {"form":form})
+
+
+@login_required
 def home(request):
    user = request.user
-
    return render(request, 'slobg_app/home.html', {user: user})
 
+@login_required
 def add_individual_hours(request):
    form = VolunteerHoursForm(request.POST or None)
    if form.is_valid():
@@ -19,6 +42,7 @@ def add_individual_hours(request):
    }
    return render(request, "slobg_app/ind_add_hours.html", context)
 
+@login_required
 def add_group_hours(request):
    form = GroupVolunteerForm(request.POST or None)
    if form.is_valid():
@@ -27,5 +51,4 @@ def add_group_hours(request):
    context = {
       'form': form
    }
-
    return render(request, 'slobg_app/group_sign_in.html', {})
