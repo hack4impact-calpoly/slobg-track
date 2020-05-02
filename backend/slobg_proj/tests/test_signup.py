0b0c8060c02e8f69a5b89eb2486a56a38843e507
Eric Newcomer <enewcomer23@gmail.com>
2020-05-02 10:30:04 -0700

    Remove areas of interest from test signup form

--- backend/slobg_proj/tests/test_signup.py
+++ backend/slobg_proj/tests/test_signup.py
@@ -16,7 +16,7 @@ class MyTests(TestCase):
             'phone' : "00000000000",
             'birth_date' : "1998-10-23",
             'medical_conditions' : "This is a test description. This is a test description. This is a test description.",
-            'areas_of_interest' : "This is a test description. This is a test description. This is a test description.",
+            # 'areas_of_interest' : "This is a test description. This is a test description. This is a test description.",
             'volunteer_waiver_and_release' : "Test Signature",
             'esignature_date' : '2020-04-12',
         }
