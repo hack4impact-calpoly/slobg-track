8bac9ff5af637f3ebd31666814f684acd633f9d9
Eric Newcomer <enewcomer23@gmail.com>
2020-09-11 15:35:44 -0700

    edit settings

--- backend/slobg_proj/slobg_proj/settings.py
+++ backend/slobg_proj/slobg_proj/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'slobg_proj.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -74,9 +73,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
